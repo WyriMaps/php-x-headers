f3359148a27eb53108d62a3a05967daeb12c8070
Benny Chan <bennycchan@gmail.com>
2017-10-27 19:18:30 -0400

    Added some alliance

--- src/Alliance.php
+++ src/Alliance.php
@@ -8,6 +8,11 @@ final class Alliance
     {
         return [
             'X-Stormwind-Guard' => 'King\'s honor friend',
+            'X-Dwarf' => 'For Khaz Modan!',
+            'X-Human' => 'For the Alliance!',
+            'X-Varian-Wrynn' => 'Stormwind honors your service to the Alliance',
+            'X-Arthas-Menethil' => 'For Lordaeron!',
+            'X-Magni-Bronzebeard' => 'Feel the fury of the mountain!',
         ];
     }
 }
