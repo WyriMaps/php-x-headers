edbb4c931d98cc7daa1ae89cf8978dac22ef9938
Quah Shiwei Dave <sdquah88@gmail.com>
2017-10-28 09:39:59 +0800

    Add new quotes for alliance

--- src/Alliance.php
+++ src/Alliance.php
@@ -8,6 +8,11 @@ final class Alliance
     {
         return [
             'X-Stormwind-Guard' => 'King\'s honor friend',
+            'X-Tyrande-Whisperwind' => 'Anu\'dora!',
+            'X-Draenei' => 'The Naaru have not forgotten us.',
+            'X-Gnome' => 'My, you\'re a tall one!',
+            'X-Night-Elf' => 'Elune be with you.',
+            'X-Worgen' => 'What\'s your story?',
         ];
     }
 }
